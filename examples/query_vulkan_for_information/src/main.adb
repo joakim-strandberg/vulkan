62a415e8e09035ae6bad99f2147b364f8b399f91
Joakim Strandberg <joakimds@kth.se>
2016-10-10 22:58:48 +0200

    Aligned assignments

--- examples/query_vulkan_for_information/src/main.adb
+++ examples/query_vulkan_for_information/src/main.adb
@@ -13,20 +13,20 @@ procedure Main is
 
    Info : aliased Vk.Instance_Create_Info_T;
 begin
-   Application_Info.Stype := Vk.STRUCTURE_TYPE_APPLICATION_INFO;
-   Application_Info.Papplication_Name := Application_Name;
+   Application_Info.Stype               := Vk.STRUCTURE_TYPE_APPLICATION_INFO;
+   Application_Info.Papplication_Name   := Application_Name;
    Application_Info.Application_Version := 16#010000#;
-   Application_Info.Pengine_Name := Engine_Name;
-   Application_Info.Engine_Version := 16#010000#;
-   Application_Info.Api_Version := 1; -- VK_API_VERSION_1_0 = 1
+   Application_Info.Pengine_Name        := Engine_Name;
+   Application_Info.Engine_Version      := 16#010000#;
+   Application_Info.Api_Version         := 1; -- VK_API_VERSION_1_0 = 1
 
-   Info.Stype := Vk.STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
-   Info.Pnext := Vk.Null_Void_Pointer;
-   Info.Flags := 0;
-   Info.Papplication_Info := Application_Info'Unchecked_Access;
-   Info.Enabled_Layer_Count := 0;
-   Info.Pp_Enabled_Layer_Names := null;
-   Info.Enabled_Extension_Count := 0;
+   Info.Stype                      := Vk.STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+   Info.Pnext                      := Vk.Null_Void_Pointer;
+   Info.Flags                      := 0;
+   Info.Papplication_Info          := Application_Info'Unchecked_Access;
+   Info.Enabled_Layer_Count        := 0;
+   Info.Pp_Enabled_Layer_Names     := null;
+   Info.Enabled_Extension_Count    := 0;
    Info.Pp_Enabled_Extension_Names := null;
 
    Result := Vk.Create_Instance (Pcreate_Info => Info'Unchecked_Access,
